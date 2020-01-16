7bcfdd8f7a37bc7b7484b00e7c37bdc4b6683503
clementval <clementval@gmail.com>
2020-01-16 09:42:34 -0500

    Add basic test

---
+++ test/semantics/acc-clause-validity.f90
@@ -0,0 +1,32 @@
+! OPTIONS: -fopenacc
+
+! Check OpenACC clause validity for the following directives:
+!
+!    2.5 PARALLEL construct
+!    2.7.1 Loop construct
+!    ...
+
+program openacc_clause_validity
+
+  implicit none
+
+  integer :: i
+  integer :: N = 256
+  real(8) :: a(256)
+
+  !$acc parallel device_type(*) num_gangs(2)
+  !$acc loop
+  do i = 1, N
+    a(i) = 3.14
+  end do
+  !$acc end parallel
+
+  !ERROR: Clause IF is not allowed after clause DEVICE_TYPE on the PARALLEL directive
+  !$acc parallel device_type(*) if(.TRUE.)
+  !$acc loop
+  do i = 1, N
+    a(i) = 3.14
+  end do
+  !$acc end parallel
+
+end program openacc_clause_validity
